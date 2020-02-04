9766ae81a3614ecf416a7e7807f2ef3d887cfb8e
Moises Santos <moises0rocha@gmail.com>
2020-02-04 15:16:52 -0300

    Add plot loss function and test MAE

--- recurrent_tensorflowkeras/practice_2.py
+++ recurrent_tensorflowkeras/practice_2.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_virtual_device_configuration(gpus[0],[tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
@@ -15,7 +16,7 @@ def vanilla_lstm(X, hidden, n_steps, n_features):
     model = keras.Sequential()
     model.add(keras.layers.LSTM(units = hidden, activation = 'relu', input_shape = (n_steps, n_features)))
     model.add(keras.layers.Dense(units = 1))
-    model.compile(optimizer = keras.optimizers.Adam(), loss = 'mean_squared_error')
+    model.compile(optimizer = keras.optimizers.Adam(learning_rate=0.01), loss = 'mean_squared_error')
     return X, model
 
 def stacked_lstm(X, hidden, n_steps, n_features):
@@ -24,7 +25,7 @@ def stacked_lstm(X, hidden, n_steps, n_features):
     model.add(keras.layers.LSTM(hidden, activation='relu', return_sequences=True, input_shape=(n_steps, n_features)))
     model.add(keras.layers.LSTM(hidden, activation='relu'))
     model.add(keras.layers.Dense(units = 1))
-    model.compile(optimizer = keras.optimizers.Adam(), loss = 'mean_squared_error')
+    model.compile(optimizer = keras.optimizers.Adam(learning_rate=0.01), loss = 'mean_squared_error')
     return X, model
 
 def bidirectional_lstm(X, hidden, n_steps, n_features):
@@ -33,7 +34,7 @@ def bidirectional_lstm(X, hidden, n_steps, n_features):
     model.add(keras.layers.Bidirectional(keras.layers.LSTM(hidden, activation='relu', return_sequences = True), input_shape=(n_steps, n_features)))
     model.add(keras.layers.Bidirectional(keras.layers.LSTM(units = hidden, activation='relu')))
     model.add(keras.layers.Dense(units = 1))
-    model.compile(optimizer = keras.optimizers.Adam(), loss = 'mean_squared_error')
+    model.compile(optimizer = keras.optimizers.Adam(learning_rate=0.01), loss = 'mean_squared_error')
     return X, model
 
 
@@ -52,14 +53,14 @@ def split_sequence(sequence, n_steps):
     return array(X), array(y)
 
 
-df = pd.read_csv("~/Documentos/recurrent_practices/recurrent_tensorflowkeras/data/fa_increasing_trend.data", header = None)
+df = pd.read_csv("~/Documentos/recurrent_practices/recurrent_tensorflowkeras/data/stockmarket_f.data", header = None)
 
 raw_seq = df.values
 plt.figure(1)
 plt.plot(raw_seq)
 plt.show()
 
-n_steps = 25
+n_steps = 20
 X, y = split_sequence(raw_seq, n_steps)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, shuffle = False)
@@ -73,10 +74,10 @@ X_train, model = bidirectional_lstm(X_train, hidden, n_steps, n_features)
 #X_train, model = stacked_lstm(X_train, hidden, n_steps, n_features)
 
 
-model.fit(X_train, y_train, epochs=20,
-    batch_size=16,
+history = model.fit(X_train, y_train, epochs=200,
+    batch_size=20,
     validation_split=0.1,
-    verbose=0,
+    verbose=1,
     shuffle=False)
 
 x_input = X_test
@@ -85,4 +86,15 @@ yhat = model.predict(x_input, verbose=0)
 plt.figure(2)
 plt.plot(y_test)
 plt.plot(yhat)
+plt.show()
+
+print("MAE: "+str(mean_absolute_error(yhat,y_test)))
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
